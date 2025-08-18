c40a64f0f8869c9c42d53f57dc658cf75250558b
yousraamr <yousraamr000@gmail.com>
2025-08-18 18:46:03 +0200

    Update: add firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:teamup_app/routes/route_names.dart';
 import 'routes/custom_router.dart';
 
-void main() {
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
