32602f0c6681355f8fcd161581f223902b3e6870
yousraamr <yousraamr000@gmail.com>
2025-08-18 18:44:35 +0200

    Feat: Create empty home page

---
+++ lib/features/auth/presentation/home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Welcome to Home Screen',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
