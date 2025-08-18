d6594b9dfbe50500ea48513a219fb3ab17ea1c58
yousraamr <yousraamr000@gmail.com>
2025-08-18 18:48:58 +0200

    Update: add home route

--- lib/routes/route_names.dart
+++ lib/routes/route_names.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import '../features/auth/presentation/home_screen.dart';
 import '../features/auth/presentation/forgot_password_screen.dart';
 import '../features/auth/presentation/login_screen.dart';
 import '../features/auth/presentation/onboarding_screen.dart';
@@ -17,6 +17,8 @@ class CustomRouter {
         return MaterialPageRoute(builder: (_) => const LoginScreen());
       case forgetPasswordScreen:
         return MaterialPageRoute(builder: (_) => const ForgotPasswordScreen());
+      case homeScreen:
+        return MaterialPageRoute(builder: (_) => const HomeScreen());
       default:
         return MaterialPageRoute(
           builder: (_) => const Scaffold(
